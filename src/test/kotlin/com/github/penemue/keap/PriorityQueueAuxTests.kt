cac02feee726681ffb02e0f2a9a99d9dcf425d66
Vyacheslav Lukianov <lvo@intellij.net>
2016-12-03 16:27:38 +0300

    keapify tests

--- src/test/kotlin/com/github/penemue/keap/PriorityQueueAuxTests.kt
+++ src/test/kotlin/com/github/penemue/keap/PriorityQueueAuxTests.kt
@@ -1,17 +1,34 @@
 package com.github.penemue.keap
 
 import com.github.penemue.keap.PriorityQueue.KeapMiscellaneous.MIN_CAPACITY
-import org.junit.Assert
+import com.github.penemue.keap.RandomStrings.randomStringListOfSize
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import java.util.*
 
 class PriorityQueueAuxTests {
 
     @Test
+    fun keapifyRandom() {
+        testKeapify(randomStringListOfSize(100000))
+    }
+
+    @Test
+    fun keapifyOrderedAsc() {
+        testKeapify(randomStringListOfSize(100000).keapSort().asList())
+    }
+
+    @Test
+    fun keapifyOrderedDesc() {
+        testKeapify(randomStringListOfSize(100000).keapSort().asList().asReversed())
+    }
+
+    @Test
     fun minCapacity() {
-        Assert.assertEquals(MIN_CAPACITY, PriorityQueue<Int>().capacity)
+        assertEquals(MIN_CAPACITY, PriorityQueue<Int>().capacity)
         repeat(MIN_CAPACITY - 1, {
-            Assert.assertEquals(MIN_CAPACITY, PriorityQueue<Int>(it + 1).capacity)
+            assertEquals(MIN_CAPACITY, PriorityQueue<Int>(it + 1).capacity)
         })
     }
 
@@ -19,19 +36,25 @@ class PriorityQueueAuxTests {
     fun stability() {
         val q = PriorityQueue<StableComparable>()
         val rnd = Random()
-        repeat(1000000, { q.offer(StableComparable(rnd.nextInt(100))) })
+        repeat(100000, { q.offer(StableComparable(rnd.nextInt(100))) })
 
-        Assert.assertEquals(1000000, q.size)
+        assertEquals(100000, q.size)
 
         var prev = q.poll()
         var current = q.poll()
         while (current != null) {
             if (current.compareTo(prev ?: throw NullPointerException()) == 0) {
-                Assert.assertTrue(prev.index < current.index)
+                assertTrue(prev.index < current.index)
             }
             prev = current
             current = q.poll()
         }
+    }
+
+    private fun testKeapify(input: List<String>) {
+        val cmp = CountingComparator<String>(Comparator(String::compareTo))
+        input.keapify(cmp)
+        assertEquals(input.size - 1, cmp.count)
     }
 
     private class StableComparable(var value: Int) : Comparable<StableComparable> {
