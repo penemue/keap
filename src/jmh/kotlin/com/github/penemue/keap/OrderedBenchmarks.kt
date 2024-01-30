32832850dffa284d7c9299d331b27fe19972f512
Vyacheslav Lukianov <vyacheslav.lukianov@gmail.com>
2024-01-30 18:21:02 +0300

    Update ©️

--- src/jmh/kotlin/com/github/penemue/keap/OrderedBenchmarks.kt
+++ src/jmh/kotlin/com/github/penemue/keap/OrderedBenchmarks.kt
@@ -1,5 +1,5 @@
 /**
- * Copyright 2016 - 2023 Vyacheslav Lukianov (https://github.com/penemue)
+ * Copyright 2016 - 2024 Vyacheslav Lukianov (https://github.com/penemue)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
